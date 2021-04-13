dd26b53f23f321000ff18d739cf57078e5f1c6e9
sakshibobde <sakshibo17@gmail.com>
2021-04-13 13:39:25 +0530

    first commit

---
+++ eclips_test/src/eclips_test/Test_Class.java
@@ -0,0 +1,14 @@
+package eclips_test;
+
+public class Test_Class {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Sakshi Sanjay Bobde");
+		
+
+		int a=10;
+		int b=50;
+		System.out.println(a+b);
+	}
+
+}

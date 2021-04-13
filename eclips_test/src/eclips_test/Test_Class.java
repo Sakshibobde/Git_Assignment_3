62025ac6f49f09eb3b21d04c02e410587a34d53c
sakshibobde <sakshibo17@gmail.com>
2021-04-13 13:44:06 +0530

    commit changes

--- eclips_test/src/eclips_test/Test_Class.java
+++ eclips_test/src/eclips_test/Test_Class.java
@@ -3,12 +3,12 @@ package eclips_test;
 public class Test_Class {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Sakshi Sanjay Bobde");
+		System.out.println("Sakshi Bobde");
 		
 
-		int a=10;
-		int b=50;
-		System.out.println(a+b);
+		int X=10;
+		int Y=50;
+		System.out.println(X+Y);
 	}
 
 }
